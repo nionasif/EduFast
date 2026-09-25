1de619c665456637078bf78c9ed4bff06752ca95
nionasif <nionasif121@gmail.com>
2026-09-26 00:10:41 +0600

    Fix

--- Forntend/vite.config.js
+++ Forntend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+  base: process.env.VITE_BASE_PATH || '/EduFast',    
   server: {
     proxy: {
       '/api': {
